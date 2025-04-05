1ff82df450162d050a37bcfa4d8f7ee0c1dfec2f
chrlsia <xrsia@otenet.gr>
2025-04-05 10:50:03 +0300

    enum's

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,17 @@ fn main() {
     // dbg!(&string);
     // dbg!(string_slice);
 
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
+    let head = Method::HEAD;
+    let connect = Method::CONNECT;
+    let options = Method::OPTIONS;
+    let trace = Method::TRACE;
+    let patch = Method::PATCH;
+
+
     let server = Server::new(String::from("127.0.0.1:8080"));
     server.run();
 
@@ -23,3 +34,26 @@ impl Server {
         println!("Listening on {}",self.addr);
     }
 }
+
+struct Request {
+    path: String,
+    query_string:String,
+    method: Method,
+}
+
+enum Method{
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
